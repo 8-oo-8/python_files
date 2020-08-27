0f977276eb7c6c138dec8910d786453aa64f8ec5
Personal Machine2 <u7136359@anu.edu.au>
2020-08-27 18:04:10 +0800

    Upload New Files

---
+++ unique_long.py
@@ -0,0 +1,10 @@
+def unique_long_word(wlist, wlen):
+    count = 0
+    for word in list(set(wlist)):
+        if len(word) >= wlen:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    print(unique_long_word(["yes","no","ng"],1))
