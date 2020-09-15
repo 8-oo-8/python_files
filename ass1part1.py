47cd4f94b3fe5a39ae6a93eeafd03b63c5c7a6b7
Personal Machine2 <u7136359@anu.edu.au>
2020-09-15 11:20:06 +0800

    upload

---
+++ ass1part1.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+
+def exhaustive_search_exhaustive(capacity,bookings):
+    tem = []
+    for ele in bookings:
+        tem.append(ele[1])
+    res = []
+    for i in range(1,len(tem) + 1):
+        res += list(combinations(tem, i))
+    optimizaion = 0
+    for ele in res:
+        if sum(ele) > optimizaion and sum(ele) <= capacity:
+            optimizaion = sum(ele)
+    return optimizaion             
+                
+
+
+
+if __name__ == '__main__':
+    b1 = [('Leckie', 2), ('Zalk', 3), ('Khan', 2), ('Aickelin', 6)]
+    print(exhaustive_search_exhaustive(5, b1))
+    print(exhaustive_search_exhaustive(7, b1))
+    b2 = [('tom', 8), ('dora', 8), ('huan', 8)]
+    print(exhaustive_search_exhaustive(7, b2))
+    print(exhaustive_search_exhaustive(20, b2))
---
+++ ass1part2.py
@@ -0,0 +1,21 @@
+def biggest_spend_first(capacity, bookings):
+    accS = 0
+    countC = 0
+    bookings.sort(key = lambda x : x[2], reverse = True)
+    for ele in bookings:
+        if countC + ele[1] > capacity:
+            continue
+        else:
+            countC += ele[1]
+            accS += ele[2]
+    return accS        
+
+if __name__ == '__main__':
+    b1 = [('Leckie', 2,20), ('Zalk', 3,20), ('Khan', 2,50), ('Aickelin', 6,80)]
+    print(biggest_spend_first(5, b1))
+    print(biggest_spend_first(10, b1))
+    b2 = [('tom', 8,100), ('dora', 4, 100), ('huan', 4, 100)]
+    print(biggest_spend_first(10, b2))
+    print(biggest_spend_first(3, b2))
+    b3 = [('tom', 4,100), ('dora', 4, 100), ('huan', 8, 100)]
+    print(biggest_spend_first(10,b3))
