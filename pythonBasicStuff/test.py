7ec8274c759e2a48c6474d60ee52a137acd4588b
Xingjian Leng <u7136359@anu.edu.au>
2021-02-26 09:11:27 +0800

    update the python basic stuff

--- pythonBasicStuff/test.py
+++ pythonBasicStuff/test.py
@@ -1,8 +1,30 @@
-age = 20
-if age < 4:
-    price = 0
-elif age < 17:
-    price = 5
-elif age >= 19:
-    price = 10
-print(price)
+def get_relevant(docs, query):
+    if not (docs and query):
+        return None
+    else:
+        li = {}
+        for i in docs.keys():
+            words = docs[i].split(" ")
+            count = 0
+            total = 0
+            for word in words:
+                total += 1
+                if word in query:
+                    count += 1
+            li[i] = count / total
+
+        sort_li = []
+        max_num = max(li.values())
+        for doc, curr_n in li.items():
+            if curr_n == max_num and curr_n != 0:
+                sort_li.append(doc)
+            else:
+                continue
+        return sort_li
+
+
+print(get_relevant({'doc1': 'A cat sat on the mat', 'doc2': 'A cat had a kitten', 'doc3': 'I love ice cream'},
+                   ['cat', 'cats', 'kitten']))
+print(get_relevant({}, []))
+print(get_relevant({'doc1': 'A dog and a cat', 'doc2': 'The dogs and the cats', 'doc3': 'I love ice cream'},
+                   ['cat', 'cats', 'dog', 'dogs']))
